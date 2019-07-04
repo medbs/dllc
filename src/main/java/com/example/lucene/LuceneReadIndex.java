7f23b72c71eb9f7fd833c0e333d525d06bb76d8b
medbs <med.boulaares@gmail.com>
2019-07-04 14:30:52 +0100

    adding synonym analyzer

--- src/main/java/com/example/lucene/LuceneReadIndex.java
+++ src/main/java/com/example/lucene/LuceneReadIndex.java
@@ -1,6 +1,5 @@
 package com.example.lucene;
-
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -11,18 +10,16 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.nio.file.Paths;
 
 @Component
 public class LuceneReadIndex {
 
-    private static String INDEX_DIR = "index" ;
+    private static String INDEX_DIR = "index";
 
-    public  void Read() throws Exception {
+    public void Read() throws Exception {
         IndexSearcher searcher = createSearcher();
 
         //Search by ID
@@ -36,7 +33,7 @@ public class LuceneReadIndex {
         }
 
         //Search by firstName
-        TopDocs foundDocs2 = searchByFirstName("Chuck", searcher);
+        TopDocs foundDocs2 = searchByFirstName("wahidel9arn", searcher);
 
         System.out.println("Total Results :: " + foundDocs2.totalHits);
 
@@ -54,18 +51,19 @@ public class LuceneReadIndex {
     }
 
     private static TopDocs searchByFirstName(String firstName, IndexSearcher searcher) throws Exception {
-        QueryParser qp = new QueryParser("firstName", new StandardAnalyzer());
+        QueryParser qp = new QueryParser("firstName", new WhitespaceAnalyzer());
         Query firstNameQuery = qp.parse(firstName);
         TopDocs hits = searcher.search(firstNameQuery, 10);
         return hits;
     }
 
     private static TopDocs searchById(Integer id, IndexSearcher searcher) throws Exception {
-        QueryParser qp = new QueryParser("id", new StandardAnalyzer());
+        QueryParser qp = new QueryParser("id", new WhitespaceAnalyzer());
         Query idQuery = qp.parse(id.toString());
         TopDocs hits = searcher.search(idQuery, 10);
         return hits;
     }
+
 
 
 }
--- src/main/java/com/example/lucene/LuceneWriteIndex.java
+++ src/main/java/com/example/lucene/LuceneWriteIndex.java
@@ -1,6 +1,12 @@
 package com.example.lucene;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -8,30 +14,29 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.lucene.util.CharsRef;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Configuration
 public class LuceneWriteIndex {
 
-      private static String INDEX_DIR = "index" ;
+    private static String INDEX_DIR = "index";
 
-    public void Write() throws Exception
-    {
+    public void Write() throws Exception {
         IndexWriter writer = createWriter();
         List<Document> documents = new ArrayList<>();
 
-        Document document1 = createDocument(1, "Karka", "Dan", "karkadance.com");
+        Document document1 = createDocument(1, "karkadan", "originalfann", "karkadance.com");
         documents.add(document1);
 
-        Document document2 = createDocument(2, "Chuck", "Norris", "chuck.com");
+        Document document2 = createDocument(2, "mamadou", "segpa", "segpa.com");
         documents.add(document2);
 
         //clean everything first
@@ -42,21 +47,44 @@ public class LuceneWriteIndex {
         writer.close();
     }
 
-    private static Document createDocument(Integer id, String firstName, String lastName, String website)
-    {
+    private static Document createDocument(Integer id, String firstName, String lastName, String website) {
         Document document = new Document();
-        document.add(new StringField("id", id.toString() , Field.Store.YES));
-        document.add(new TextField("firstName", firstName , Field.Store.YES));
-        document.add(new TextField("lastName", lastName , Field.Store.YES));
-        document.add(new TextField("website", website , Field.Store.YES));
+        document.add(new StringField("id", id.toString(), Field.Store.YES));
+        document.add(new TextField("firstName", firstName, Field.Store.YES));
+        document.add(new TextField("lastName", lastName, Field.Store.YES));
+        document.add(new TextField("website", website, Field.Store.YES));
         return document;
     }
 
-    private static IndexWriter createWriter() throws IOException
-    {
+    private static IndexWriter createWriter() throws Exception {
         FSDirectory dir = FSDirectory.open(Paths.get(INDEX_DIR));
-        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+        Analyzer analyzer = createAnalyzer();
+        //IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer()); //default analyzer
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter writer = new IndexWriter(dir, config);
         return writer;
     }
+
+
+    private static Analyzer createAnalyzer() throws Exception {
+        SynonymMap.Builder builder = new SynonymMap.Builder();
+        builder.add(new CharsRef("karkadan"), new CharsRef("wahidel9arn"), true);
+        final SynonymMap map = builder.build();
+
+        Analyzer indexTimeAnalyzer = new Analyzer() {
+            @Override
+            protected TokenStreamComponents createComponents(String fieldName) {
+                Tokenizer tokenizer = new WhitespaceTokenizer();
+                SynonymGraphFilter synFilter = new SynonymGraphFilter(tokenizer, map, true);
+                return new TokenStreamComponents(tokenizer, synFilter);
+            }
+        };
+
+        Map<String, Analyzer> perFieldAnalyzers = new HashMap<>();
+        perFieldAnalyzers.put("year", new KeywordAnalyzer());
+        Analyzer analyzer = new PerFieldAnalyzerWrapper(indexTimeAnalyzer, perFieldAnalyzers);
+        return analyzer;
+
+    }
+
 }
